7d003d430a70971ab5cd141861bc7455b625ccec
Alessandro Ferraro <ale_ferraro@libero.it>
2020-12-22 16:31:14 +0100

    PHP Snack 1

---
+++ index.php
@@ -0,0 +1,63 @@
+<?php
+
+/*
+PHP Snack 1:
+Creiamo un array 'matches' contenente altri array i quali rappresentano delle partite di basket di un’ipotetica tappa del calendario. Ogni array della partita avrà una squadra di casa e una squadra ospite, punti fatti dalla squadra di casa e punti fatti dalla squadra ospite.
+Stampiamo a schermo tutte le partite con questo schema:
+Olimpia Milano - Cantù | 55-60
+
+
+PHP Snack 2:
+Passare come parametri GET name, mail e age everificare (cercando i metodi che non conosciamo nella documentazione) che:
+1. name sia più lungo di 3 caratteri,
+2. che mail contenga un punto e una chiocciola
+3. e che age sia un numero.
+Se tutto è ok stampare “Accesso riuscito”, altrimenti “Accesso negato”
+*/
+
+
+
+
+// PHP Snack 1
+
+$matches = [
+    [
+        'casa' => 'Olimpia Milano',
+        'ospite' => 'Dinamo Sassari',
+        'punteggio_casa' => 102,
+        'punteggio_ospite' => 86
+    ],
+    [
+        'casa' => 'Universo Treviso',
+        'ospite' => 'Virtus Bologna',
+        'punteggio_casa' => 72,
+        'punteggio_ospite' => 98
+    ],
+    [
+        'casa' => 'Venezia',
+        'ospite' => 'Varese',
+        'punteggio_casa' => 88,
+        'punteggio_ospite' => 77
+    ],
+    [
+        'casa' => 'Cantù',
+        'ospite' => 'Brescia',
+        'punteggio_casa' => 89,
+        'punteggio_ospite' => 92
+    ],
+    [
+        'casa' => 'Fortitudo Bologna',
+        'ospite' => 'Cremona',
+        'punteggio_casa' => 85,
+        'punteggio_ospite' => 71
+    ]
+];
+
+echo "<strong>PHP Snack 1</strong><br>";
+
+for ($i = 0; $i < 5; $i++){
+    echo $matches[$i]['casa'] . " - " . $matches[$i]['ospite'] . " | " . $matches[$i]['punteggio_casa'] . " - " . $matches[$i]['punteggio_ospite'] . "<br>";
+}
+
+
+?>
